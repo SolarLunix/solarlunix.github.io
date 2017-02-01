7009c960ec0693b6eb1f319d10b2fc3d8b8192a9
SolarLunix <solarlunix@gmail.com>
2017-02-01 21:38:02 +0000

    Update
    Added DOMmanip3 both html and js
    Updated DOMmanip2.js
    Added link to DOMmanip3 to webDev homepage.

--- WebDev/DOMmanip2.js
+++ WebDev/DOMmanip2.js
@@ -1,15 +1,28 @@
-var h1 = document.querySelector("h1");
+var tag = document.getElementById("highlight");
+tag.style.background = "yellow";
 
-h1.style.color = "pink";
+//Selects multiple items and makes them "list like" but you cannot go through them with a forEach loop
+var tags = document.getElementsByClassName("bolded");
+var allListItems = document.getElementsByTagName("li");
+var allH1 = document.getElementsByTagName("h1");
 
-var body = document.querySelector("body");
-var isBlue = false;
+console.log("Bolded tags: " + tags.length);
+console.log("All List Items: " + allListItems.length);
+console.log("H1s: " + allH1.length);
 
-setInterval(function(){
-	if(isBlue){
-		body.style.background = "white";
-	}else{
-		body.style.background = "#adccff";
-	}
-	isBlue = !isBlue;
-}, 1000);
+//Returns only one element
+var newTag = document.querySelector("#highlight");
+newTag.style.color = "purple";
+
+var firstBolded = document.querySelector(".bolded");
+firstBolded.style.color = "red";
+
+var firstH1 = document.querySelector("h1");
+firstH1.style.color = "pink";
+
+var specialLink = document.querySelector("li a.special");
+specialLink.style.color = "white";
+
+//Returns multiple elements 
+var bolded = document.querySelectorAll(".bolded");
+console.log("Bolded tags: " + bolded.length);
---
+++ WebDev/DOMmanip3.js
@@ -0,0 +1,2 @@
+var try1 = document.getElementByID("first");
+console.log(try1);

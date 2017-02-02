cc02fa569c3a58428a416a160b3a274adef466c6
SolarLunix <solarlunix@gmail.com>
2017-02-02 11:31:47 +0000

    Updated DOMmanip6.js
    Created a more robust image rotator so that more images could easily be
    added later.

--- WebDev/DOMmanip6.js
+++ WebDev/DOMmanip6.js
@@ -2,13 +2,14 @@ var count = 0;
 
 var img = document.getElementsByTagName("img")[0];
 
+var imgs = ["https://solarlunix.github.io/Images/RyanMelissa1.jpg",
+			"https://solarlunix.github.io/Images/RockoMelissa1.jpg"]
+
 setInterval(function(){
-	if(count === 0){
-		img.setAttribute("src", "https://solarlunix.github.io/Images/RyanMelissa1.jpg");
-	}else if(count === 1){
-		img.setAttribute("src", "https://solarlunix.github.io/Images/RockoMelissa1.jpg");
+	if(count === (imgs.length - 1)){
 		count = -1;
 	}
-
 	count++;
+
+	img.setAttribute("src", imgs[count]);
 }, 2500);

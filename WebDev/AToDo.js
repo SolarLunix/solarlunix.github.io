fe4805a6a9ac86576488e76d5db4e836b3cbc732
SolarLunix <solarlunix@gmail.com>
2017-02-02 17:41:58 +0000

    Finished ToDo List

---
+++ WebDev/AToDo.js
@@ -0,0 +1,15 @@
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i < lis.length; i++){
+	lis[i].addEventListener("mouseover", function(){
+		this.classList.add("hovering");
+	});
+
+	lis[i].addEventListener("mouseout", function(){
+		this.classList.remove("hovering");
+	});
+
+	lis[i].addEventListener("click", function(){
+		this.classList.toggle("done");
+	})
+}

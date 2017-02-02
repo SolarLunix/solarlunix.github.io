e782984a02e94ce9250918ae87baa90f0a156b06
SolarLunix <solarlunix@gmail.com>
2017-02-02 14:08:14 +0000

    Updates
    Fixed a few code pieces,

--- WebDev/DOMmanip7.js
+++ WebDev/DOMmanip7.js
@@ -2,9 +2,6 @@ var checks = document.getElementsByTagName("li");
 var checks_hold = [];
 
 for(var i = 0; i < checks.length; i++){
-	checks[i].classList.add("done");
-	checks[i].classList.toggle("done");
-
 	checks[i].addEventListener("click", function(){
 		this.classList.toggle("done");
 		console.log(checks[i]);

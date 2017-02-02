3f35a369dba54ef8ade35a0c84674760c7614519
SolarLunix <solarlunix@gmail.com>
2017-02-02 11:58:24 +0000

    Update
    Added DOMmanip7

---
+++ WebDev/DOMmanip7.js
@@ -0,0 +1,15 @@
+var count = 0;
+
+var img = document.getElementsByTagName("img")[0];
+
+var imgs = ["https://solarlunix.github.io/Images/RyanMelissa1.jpg",
+			"https://solarlunix.github.io/Images/RockoMelissa1.jpg"]
+
+setInterval(function(){
+	if(count === (imgs.length - 1)){
+		count = -1;
+	}
+	count++;
+
+	img.setAttribute("src", imgs[count]);
+}, 2500);

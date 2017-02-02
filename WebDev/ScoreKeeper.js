07c8ab6620d352dfedbe99ceaf756c0e7514221b
SolarLunix <solarlunix@gmail.com>
2017-02-02 15:32:59 +0000

    Update
    Finished ScoreKeeper
    Added A new ToDo List

--- WebDev/ScoreKeeper.js
+++ WebDev/ScoreKeeper.js
@@ -1,13 +1,53 @@
-var button = document.getElementsByTagName("button")[0];
-var body = document.querySelector("body");
-var blue = true;
+var p1 = document.getElementById("p1");
+var p1display = document.getElementById("p1score");
+var p1score = 0;
 
-button.addEventListener("click", function(){
-	if(blue){
-		body.style.backgroundColor = "pink";
-	}else{
-		body.style.backgroundColor = "cyan";
+var p2 = document.getElementById("p2");
+var p2display = document.getElementById("p2score");
+var p2score = 0;
+
+var reset = document.getElementById("reset");
+var scoreSelect = document.getElementById("maxpoints");
+var maxScore = 5;
+
+var gameOver = false;
+
+p1.addEventListener("click", function(){
+	if(!gameOver){
+		p1score++;
+		updateScores();
+		if(p1score >= maxScore){
+			p1display.classList.add("winner");
+			gameOver = true;
+		}
 	}
-	console.log("click");
-	blue = !blue;
 });
+
+p2.addEventListener("click", function(){
+	if(!gameOver){
+		p2score++;
+		updateScores();
+		if(p2score >= maxScore){
+			p2display.classList.add("winner");
+			gameOver = true;
+		}
+	}
+});
+
+reset.addEventListener("click", function(){
+	p1score = 0;
+	p2score = 0;
+	updateScores();
+	gameOver = false;
+	p1display.classList.remove("winner");
+	p2display.classList.remove("winner");
+});
+
+scoreSelect.addEventListener("change", function(){
+	maxScore = Number(scoreSelect.value);
+});
+
+function updateScores(){
+	p1display.textContent = p1score;
+	p2display.textContent = p2score;
+}

037242ae495e90f8250ed56a34bccf2b1b5b5f5b
SolarLunix <solarlunix@gmail.com>
2017-02-04 21:18:14 +0000

    Update jDemo6
    Removed unneeded CSS code.
    Made entering text change the div and erase the text in the field.

---
+++ WebDev/jDemo6.js
@@ -0,0 +1,20 @@
+var colours = ["red", "blue", "green", "purple"];
+
+$("h1").on("click", function(){
+	colour = colours[Math.floor(Math.random() * colours.length)];
+	$(this).css("color", colour);
+});
+
+$("h1").on("dblclick", function(){
+	colour = colours[Math.floor(Math.random() * colours.length)];
+	$("button").css("color", colour);
+	console.log("double");
+});
+
+$("input").on("keypress", function(event){
+	if(event.which === 13){
+		var text = $(this).val();
+		$(this).val("");
+		$("#text").text(text); 
+	}
+})

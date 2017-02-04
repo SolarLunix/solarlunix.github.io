fc19f40f86b8c223ca9ea37e649a3b56d2d4012e
SolarLunix <solarlunix@gmail.com>
2017-02-04 11:23:11 +0000

    added jDemo1 and jDemo2

---
+++ WebDev/jDemo1.js
@@ -0,0 +1,12 @@
+var lis = $("li");
+
+lis.css("backgroundColor", "pink");
+
+var style = {
+	color: "red",
+	backgroundColor: "black",
+	border: "1px dashed green",
+	textAlign: "center"
+};
+
+$("h1").css(style);
---
+++ WebDev/jDemo2.js
@@ -0,0 +1,11 @@
+var allDivs = $("div");
+allDivs.css("backgroundColor", "purple");
+
+var highlight = $(".highlight");
+highlight.css("width", "200px");
+
+var third = $("#third");
+third.css("border", "4px dashed orange");
+
+var first = $("div:first-of-type");
+first.css("color", "pink");

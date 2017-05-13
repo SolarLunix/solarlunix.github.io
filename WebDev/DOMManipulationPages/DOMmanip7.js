3dfc799f5ad3a1e14b2b53ae727e205261a5f0ec
Melissa Melaugh <solarlunix@gmail.com>
2017-05-13 19:42:57 +0000

    Deleted WebDev folder

--- WebDev/DOMManipulationPages/DOMmanip7.js
+++
@@ -1,9 +0,0 @@
-var checks = document.getElementsByTagName("li");
-var checks_hold = [];
-
-for(var i = 0; i < checks.length; i++){
-	checks[i].addEventListener("click", function(){
-		this.classList.toggle("done");
-		console.log(checks[i]);
-	});
-}

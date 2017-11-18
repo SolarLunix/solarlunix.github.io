6cbc61aa4c6ae24754a7d2f5da0be534d63d0445
SolarLunix <solarlunix@gmail.com>
2017-11-18 17:17:56 +0000

    Updated CV

---
+++ Assets/cv.js
@@ -0,0 +1,186 @@
+$("a[href='#awards']").on("click", function(e){
+    $("#awardbutton").addClass("hide");
+    $("#awards").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closeAwards']").on("click", function(e){
+    $("#awardbutton").removeClass("hide");
+    $("#awards").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#lyitGrades']").on("click", function(e){
+    $("#lyitgradebutton").addClass("hide");
+    $("#lyitgrades").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closeLyitGrades']").on("click", function(e){
+    $("#lyitgradebutton").removeClass("hide");
+    $("#lyitgrades").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccGrades']").on("click", function(e){
+    $("#cccgradebutton").addClass("hide");
+    $("#cccgrades").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closeCccGrades']").on("click", function(e){
+    $("#cccgradebutton").removeClass("hide");
+    $("#cccgrades").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#lyitfall']").on("click", function(e){
+    $(".lyitgrades").addClass("hide");
+    $("#lyitfall").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#lyitsummer']").on("click", function(e){
+    $(".lyitgrades").addClass("hide");
+    $("#lyitsummer").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccap']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccap").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccfall10']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccfall10").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccspring11']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccspring11").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccsummer11']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccsummer11").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccfall11']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccfall11").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccspring12']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccspring12").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccfall12']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccfall12").removeClass("hide");
+    e.preventDefault()
+});
+
+$("a[href='#cccspring13']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccspring13").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccfall13']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccfall13").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccspring14']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccspring14").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccsummer16']").on("click", function(e){
+    $(".cccgrades").addClass("hide");
+    $("#cccsummer16").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#uuproj']").on("click", function(e){
+    $("#uuprojbutton").addClass("hide");
+    $("#uuproj").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closeuuproj']").on("click", function(e){
+    $("#uuprojbutton").removeClass("hide");
+    $("#uuproj").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#pramproj']").on("click", function(e){
+    $("#pramprojbutton").addClass("hide");
+    $("#pramproj").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closepramproj']").on("click", function(e){
+    $("#pramprojbutton").removeClass("hide");
+    $("#pramproj").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccresearchproj']").on("click", function(e){
+    $("#cccresearchprojbutton").addClass("hide");
+    $("#cccresearchproj").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closecccresearchproj']").on("click", function(e){
+    $("#cccresearchprojbutton").removeClass("hide");
+    $("#cccresearchproj").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#kathiproj']").on("click", function(e){
+    $("#kathiprojbutton").addClass("hide");
+    $("#kathiproj").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closekathiproj']").on("click", function(e){
+    $("#kathiprojbutton").removeClass("hide");
+    $("#kathiproj").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#cccmproj']").on("click", function(e){
+    $("#cccmprojbutton").addClass("hide");
+    $("#cccmproj").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closecccmproj']").on("click", function(e){
+    $("#cccmprojbutton").removeClass("hide");
+    $("#cccmproj").addClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#mstarproj']").on("click", function(e){
+    $("#mstarprojbutton").addClass("hide");
+    $("#mstarproj").removeClass("hide");
+    e.preventDefault();
+});
+
+$("a[href='#closemstarproj']").on("click", function(e){
+    $("#mstarprojbutton").removeClass("hide");
+    $("#mstarproj").addClass("hide");
+    e.preventDefault();
+});
+
